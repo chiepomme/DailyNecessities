72c10ec19a3e854592b9258b07ccb29cf46e7391
chiepomme <chiepomme@users.noreply.github.com>
2022-05-21 19:56:48 +0900

    Create SnapToTerrainUtility.cs

---
+++ SnapToTerrainUtility.cs
@@ -0,0 +1,29 @@
+#if UNITY_EDITOR
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace Namespace
+{
+    public static class SnapToTerrainUtility
+    {
+        [MenuItem("GameObject/Snap to Terrain %&q")]
+        public static void SnapToTerrain()
+        {
+            foreach (var transform in Selection.transforms)
+            {
+                var terrainHits = Physics
+                                    .RaycastAll(transform.position + Vector3.up, Vector3.down, float.MaxValue)
+                                    .OrderBy(hit => (transform.position - hit.point).sqrMagnitude)
+                                    .Where(hit => hit.collider is TerrainCollider)
+                                    .ToArray();
+
+                if (terrainHits.Any())
+                {
+                    transform.position = terrainHits.First().point;
+                }
+            }
+        }
+    }
+}
+#endif
